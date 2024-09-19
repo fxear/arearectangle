2508b6ff1d09ef6b89b8e6fbe0ed3d8ec1345688
fxear <40257769@hruc.ac.uk>
2024-09-19 11:52:01 +0100

    Add files via upload

---
+++ rectangle.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+            float width = Convert.ToSingle(Console.ReadLine()); //the width of the rectangle
+            float height = Convert.ToSingle(Console.ReadLine()); //the height of the rectangle
+
+            Console.WriteLine(width * height); //do the width x the height for the area
+    }
+}
